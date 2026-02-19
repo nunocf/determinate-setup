cb50ad4a24ba2c0e773a89f77ebe0653686ad0c3
nunocf <nunogcferreira@gmail.com>
2026-02-19 16:31:51 +0000

    added git aliases, finally

--- home/git.nix
+++ home/git.nix
@@ -8,13 +8,27 @@
     lfs.enable = true;
 
     ignores = [ "**/.DS_STORE" ];
+    aliases = {
+      aa = "add -all";
+      ap = "add --patch";
+      amend = "commit --amend";
+      ci = "commit";
+      co = "checkout";
+      dc = "diff --cached";
+      di = "diff";
+      glog = "log --oneline";
+      publish = "push -u origin HEAD";
+      root = "rev-parse --show-toplevel";
+      st = "status";
+      yoda = "push --force-with-lease";
+    };
 
     extraConfig = {
       github = {
         user = primaryUser;
       };
       init = {
-        defaultBranch = "main";
+        defaultBranch = "master";
       };
     };
   };
