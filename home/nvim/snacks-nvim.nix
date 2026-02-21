390cd73ae3ec89c00604577413e80cd4916238fe
nunocf <nunogcferreira@gmail.com>
2026-02-21 15:41:36 +0000

    added bigfile and quickfile to snacks config

--- home/nvim/snacks-nvim.nix
+++ home/nvim/snacks-nvim.nix
@@ -2,6 +2,8 @@
   enable = true;
   setupOpts = {
     dashboard = import ./dashboard.nix;
+    bigfile.enable = true;
+    quickfile.enable = true;
     notifier.enable = true;
     indent.enable = true;
     scope.enable = true;
