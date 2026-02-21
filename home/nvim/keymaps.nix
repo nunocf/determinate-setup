5d1a195b87d20830f6b3a0c5b7118531f122291e
nunocf <nunogcferreira@gmail.com>
2026-02-21 23:34:59 +0000

    more keymaps

--- home/nvim/keymaps.nix
+++ home/nvim/keymaps.nix
@@ -50,6 +50,19 @@
     action = "<cmd>lua Snacks.picker.grep()<cr>";
     desc = "Live grep";
   }
+  {
+    mode = "n";
+    key = "<leader>fb";
+    action = "<cmd>lua Snacks.picker.buffers()<cr>";
+    desc = "Buffers";
+  }
+
+  {
+    mode = "n";
+    key = "<leader>fr";
+    action = "<cmd>lua Snacks.picker.recent()<cr>";
+    desc = "Recent files";
+  }
 
   # Git (example)
   {
@@ -71,5 +84,75 @@
     key = "gz";
     action = "";
     desc = "Folds";
+  }
+  # Noice group
+  {
+    key = "<leader>sn";
+    mode = "n";
+    action = "";
+    desc = "Noice";
+  }
+
+  {
+    key = "<leader>snl";
+    mode = "n";
+    action = "<cmd>Noice last<cr>";
+    desc = "Noice Last Message";
+  }
+
+  {
+    key = "<leader>snh";
+    mode = "n";
+    action = "<cmd>Noice history<cr>";
+    desc = "Noice History";
+  }
+
+  {
+    key = "<leader>sna";
+    mode = "n";
+    action = "<cmd>Noice all<cr>";
+    desc = "Noice All";
+  }
+
+  {
+    key = "<leader>snd";
+    mode = "n";
+    action = "<cmd>Noice dismiss<cr>";
+    desc = "Dismiss Notifications";
+  }
+  # Trouble group
+  {
+    key = "<leader>x";
+    mode = "n";
+    action = "";
+    desc = "Diagnostics";
+  }
+
+  {
+    key = "<leader>xx";
+    mode = "n";
+    action = "<cmd>Trouble diagnostics toggle<cr>";
+    desc = "Diagnostics";
+  }
+
+  {
+    key = "<leader>xw";
+    mode = "n";
+    action = "<cmd>Trouble workspace_diagnostics<cr>";
+    desc = "Workspace Diagnostics";
+  }
+
+  {
+    key = "<leader>xd";
+    mode = "n";
+    action = "<cmd>Trouble document_diagnostics<cr>";
+    desc = "Document Diagnostics";
+  }
+
+  {
+    key = "<leader>xq";
+    mode = "n";
+    action = "<cmd>Trouble quickfix<cr>";
+    desc = "Quickfix";
   }
 ]
