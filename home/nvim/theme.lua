a3600d3df0015bc58c0a89ff3df829f7085f7a70
nunocf <nunogcferreira@gmail.com>
2026-02-23 15:32:37 +0000

    fixed color of path in picker

--- home/nvim/theme.lua
+++ home/nvim/theme.lua
@@ -1,8 +1,5 @@
-vim.api.nvim_set_hl(0, "SnacksPickerPath", { link = "Directory" })
-vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "Directory" })
-vim.api.nvim_set_hl(0, "SnacksPickerDim", { link = "LineNr" })
-
 local function fix_snacks_links()
+	-- Snacks notifier links
 	local map = {
 		Error = "DiagnosticSignError",
 		Warn = "DiagnosticSignWarn",
@@ -15,6 +12,11 @@ local function fix_snacks_links()
 		vim.api.nvim_set_hl(0, "SnacksNotifierTitle" .. lvl, { link = sign })
 		vim.api.nvim_set_hl(0, "SnacksNotifierFooter" .. lvl, { link = sign })
 	end
+
+	-- Snacks picker readability (Everforest-aligned)
+	vim.api.nvim_set_hl(0, "SnacksPickerDir", { link = "Directory" })
+	vim.api.nvim_set_hl(0, "SnacksPickerPath", { link = "Directory" })
+	vim.api.nvim_set_hl(0, "SnacksPickerDim", { link = "Directory" })
 end
 
 vim.api.nvim_create_autocmd("ColorScheme", { callback = fix_snacks_links })
