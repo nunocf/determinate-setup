d252f4d73d61956d58e39c5e8f5a3c4f60b3f228
nunocf <nunogcferreira@gmail.com>
2026-05-05 20:00:31 +0100

    added dockutil so i can install terminal emulator from nix

--- hosts/work-macbook/home.nix
+++ hosts/work-macbook/home.nix
@@ -1,6 +1,8 @@
 {
+  config,
   pkgs,
   nvf,
+  lib,
   ...
 }: {
   imports = [
@@ -8,7 +10,18 @@
   ];
 
   home.packages = with pkgs; [
+    dockutil
     graphite-cli
     kitty
   ];
+
+  home.activation.pinKittyToDock = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    kitty_app="${config.home.homeDirectory}/Applications/Home Manager Apps/kitty.app"
+
+    if [ -d "$kitty_app" ]; then
+      ${pkgs.dockutil}/bin/dockutil --remove kitty --no-restart >/dev/null 2>&1 || true
+      ${pkgs.dockutil}/bin/dockutil --add "$kitty_app" --no-restart
+      /usr/bin/killall Dock >/dev/null 2>&1 || true
+    fi
+  '';
 }
