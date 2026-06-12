936edddec4344ba33cfa9aef0fc660d95f3923c6
nunocf <nunogcferreira@gmail.com>
2026-06-12 13:27:54 +0100

    added sops key alias for current deployment:

--- hosts/my-macbook/configuration.nix
+++ hosts/my-macbook/configuration.nix
@@ -11,6 +11,8 @@
     # "slack"
   ];
 
+  home-manager.backupFileExtension = "backup";
+
   # host-specific home-manager configuration
   home-manager.users.${primaryUser} = {
     imports = [
@@ -27,6 +29,7 @@
           # Source shell functions
           source ${./shell-functions.sh}
           export OPENAI_API_KEY="$(security find-generic-password -a "$USER" -s openai-api-key -w)"
+          export SOPS_AGE_KEY_FILE="$HOME/.config/sops/age/keys.txt"
 
           if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
             . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
